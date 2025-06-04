75ea41dbe88af5abe32a872129acc1c0872eb9d2
NikolaSoic <nikolasoic16@gmail.com>
2025-06-04 19:08:40 +0000

    rotacije lica kocke

--- main.cpp
+++ main.cpp
@@ -155,4 +155,234 @@ void ispisiKocku()
         cout << endl;
     }
     cout << endl;
+} 
+
+void rotirajLiceUSmjeruKazaljke(int lice)
+{
+    char temp[VELICINA][VELICINA];
+    for (int i = 0; i < VELICINA; i++)
+    {
+        for (int j = 0; j < VELICINA; j++)
+        {
+            temp[i][j] = kocka[lice][i][j];
+        }
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        for (int j = 0; j < VELICINA; j++)
+        {
+            kocka[lice][j][VELICINA - 1 - i] = temp[i][j];
+        }
+    }
+}
+
+void rotirajLiceProtivKazaljke(int lice)
+{
+    rotirajLiceUSmjeruKazaljke(lice);
+    rotirajLiceUSmjeruKazaljke(lice);
+    rotirajLiceUSmjeruKazaljke(lice);
+}
+
+// Rotacija prednjeg lica (F)
+void rotirajF()
+{
+    rotirajLiceUSmjeruKazaljke(PREDNJE);
+    char temp[VELICINA];
+    for (int i = 0; i < VELICINA; i++)
+    {
+        temp[i] = kocka[GORNJE][2][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[GORNJE][2][i] = kocka[LIJEVO][2 - i][2];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[LIJEVO][2 - i][2] = kocka[DONJE][0][2 - i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DONJE][0][2 - i] = kocka[DESNO][i][0];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DESNO][i][0] = temp[i];
+    }
+}
+
+void rotirajFPrim()
+{
+    rotirajF();
+    rotirajF();
+    rotirajF();
+}
+
+// Rotacija desnog lica (R)
+void rotirajR()
+{
+    rotirajLiceUSmjeruKazaljke(DESNO);
+    char temp[VELICINA];
+    for (int i = 0; i < VELICINA; i++)
+    {
+        temp[i] = kocka[GORNJE][i][2];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[GORNJE][i][2] = kocka[PREDNJE][i][2];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[PREDNJE][i][2] = kocka[DONJE][i][2];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DONJE][i][2] = kocka[STRAZNJE][2 - i][0];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[STRAZNJE][2 - i][0] = temp[i];
+    }
+}
+
+void rotirajRPrim()
+{
+    rotirajR();
+    rotirajR();
+    rotirajR();
+}
+
+// Rotacija gornjeg lica (U)
+void rotirajU()
+{
+    rotirajLiceUSmjeruKazaljke(GORNJE);
+    char temp[VELICINA];
+    for (int i = 0; i < VELICINA; i++)
+    {
+        temp[i] = kocka[PREDNJE][0][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[PREDNJE][0][i] = kocka[DESNO][0][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DESNO][0][i] = kocka[STRAZNJE][0][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[STRAZNJE][0][i] = kocka[LIJEVO][0][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[LIJEVO][0][i] = temp[i];
+    }
+}
+
+void rotirajUPrim()
+{
+    rotirajU();
+    rotirajU();
+    rotirajU();
+}
+
+// Rotacija lijevog lica (L)
+void rotirajL()
+{
+    rotirajLiceUSmjeruKazaljke(LIJEVO);
+    char temp[VELICINA];
+    for (int i = 0; i < VELICINA; i++)
+    {
+        temp[i] = kocka[GORNJE][i][0];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[GORNJE][i][0] = kocka[STRAZNJE][2 - i][2];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[STRAZNJE][2 - i][2] = kocka[DONJE][i][0];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DONJE][i][0] = kocka[PREDNJE][i][0];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[PREDNJE][i][0] = temp[i];
+    }
+}
+
+void rotirajLPrim()
+{
+    rotirajL();
+    rotirajL();
+    rotirajL();
+}
+
+// Rotacija donjeg lica (D)
+void rotirajD()
+{
+    rotirajLiceUSmjeruKazaljke(DONJE);
+    char temp[VELICINA];
+    for (int i = 0; i < VELICINA; i++)
+    {
+        temp[i] = kocka[PREDNJE][2][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[PREDNJE][2][i] = kocka[LIJEVO][2][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[LIJEVO][2][i] = kocka[STRAZNJE][2][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[STRAZNJE][2][i] = kocka[DESNO][2][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DESNO][2][i] = temp[i];
+    }
+}
+
+void rotirajDPrim()
+{
+    rotirajD();
+    rotirajD();
+    rotirajD();
+}
+
+// Rotacija stražnjeg lica (B)
+void rotirajB()
+{
+    rotirajLiceUSmjeruKazaljke(STRAZNJE);
+    char temp[VELICINA];
+    for (int i = 0; i < VELICINA; i++)
+    {
+        temp[i] = kocka[GORNJE][0][i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[GORNJE][0][i] = kocka[DESNO][i][2];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DESNO][i][2] = kocka[DONJE][2][2 - i];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[DONJE][2][2 - i] = kocka[LIJEVO][2 - i][0];
+    }
+    for (int i = 0; i < VELICINA; i++)
+    {
+        kocka[LIJEVO][2 - i][0] = temp[i];
+    }
+}
+
+void rotirajBPrim()
+{
+    rotirajB();
+    rotirajB();
+    rotirajB();
 }
