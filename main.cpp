15efe3b979c76fc6d34c090e4aec754b9700f54c
NikolaSoic <nikolasoic16@gmail.com>
2025-06-04 19:05:46 +0000

    Struktura za rezultat, enum za lica kocke i funkcije za vrijeme

--- main.cpp
+++ main.cpp
@@ -24,3 +24,40 @@ int ukupan_broj_poteza = 0;
 double pocetno_vrijeme = 0;
 bool igra_aktivna = false;
 char trenutni_igrac[50] = ""; // Ime trenutnog igrača
+                              // Struktura za rezultat
+struct Rezultat
+{
+    char ime[50];
+    double vrijeme;
+    int broj_poteza;
+    char datum[30];
+    bool rijeseno;
+};
+
+// Enum za lica kocke
+enum Lice
+{
+    GORNJE = 0,   // Bijela (W)
+    LIJEVO = 1,   // Narančasta (O)
+    PREDNJE = 2,  // Zelena (G)
+    DESNO = 3,    // Crvena (R)
+    STRAZNJE = 4, // Plava (B)
+    DONJE = 5     // Žuta (Y)
+};
+
+// Funkcije za vrijeme
+double trenutno_vrijeme()
+{
+    return (double)time(NULL);
+}
+
+void trenutni_datum(char *buffer)
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer, 30, "%Y-%m-%d %H:%M:%S", timeinfo);
+}
